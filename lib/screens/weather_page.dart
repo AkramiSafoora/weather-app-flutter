9286225ba30a3a45b3d65c795f58bd1163a0ddfc
AkramiSafoora <safoora.akrami@gmail.com>
2026-07-01 17:16:48 -0400

    Build weather header UI

--- lib/screens/weather_page.dart
+++ lib/screens/weather_page.dart
@@ -10,10 +10,50 @@ class WeatherPage extends StatelessWidget {
         title: const Text('Weather App'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'Toronto Weather',
-          style: TextStyle(fontSize: 24),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+        
+            children: const [
+
+              Icon(
+                Icons.wb_sunny,
+                size: 100,
+                color: Colors.orange,
+              ),
+
+              SizedBox(height: 20),
+
+              Text(
+                "Toronto",
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              SizedBox(height: 10),
+
+              Text(
+                "24°C",
+                style: TextStyle(
+                  fontSize: 52,
+                ),
+              ),
+
+              SizedBox(height: 10),
+
+              Text(
+                "Partly Cloudy",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.grey,
+                ),
+              ),
+
+            ],
+          ),
         ),
       ),
     );
